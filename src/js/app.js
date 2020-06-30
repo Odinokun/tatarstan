18a844f82d42a21dace3f258edd05c761431ab0f
odinokun <odinokun@gmail.com>
2020-06-30 11:59:17 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -6,24 +6,7 @@ $(function() {
   // begin padding top in object page
   require('./modules/objectPaddingTop')();
 
-  // begin phone mask
-  // require('./modules/maskedinput')();
-
-  // begin Scroll2Ancor
-  // require('./modules/scroll2ancor')();
-
-  // begin Slick slider
-  // require('./modules/slick')();
-
   // begin Popup
   // require('./modules/popup')();
-
-  // begin Sticky header
-  // require('./modules/stickyHeader')();
-
-  // begin Animate.css
-  //РАБОТАЕТ ТОЛЬКО С JQUERY < 3.0.0 !!!!!!!!!!!!!!!!!!
-  // require('./modules/waypoints')();
-  // require('./modules/animateCss')();
 
 });
